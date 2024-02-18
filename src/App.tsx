4accced28f3cbc01e3530ba876609035ff450ee4
daudzubaidi <daud.java@gmail.com>
2024-02-19 03:21:42 +0700

    fix: testing change test

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ function App() {
       <h1 className="text-3xl font-bold underline">
         Testing change header see if lint stage has working properly
       </h1>
+
+      <h2>this is testing tile h2, should working properly tooo</h2>
     </>
   );
 }
