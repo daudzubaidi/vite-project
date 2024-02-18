2787b4fe8c7377395d2cf37351a653e629b5f557
daudzubaidi <daud.java@gmail.com>
2024-02-19 03:46:51 +0700

    fix: app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import './App.css';
-
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Testing change header see if lint stage has working properly
-      </h1>
-
-      <h2>this is testing tile h2, should working properly tooo</h2>
+      <h1 className="text-3xl font-bold underline">Mabrur Project</h1>
+      <h2>Vite + React</h2>
     </>
   );
 }
